da434f94071c4bec412d68bff19b11b2d701e28c
AJ Pawlicki <aloysiuspaw@gmail.com>
2017-07-05 00:57:55 -0700

    Make changes to app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-// TODO
+let GroceryList = () => (
+  <ul>
+    <GroceryListItem item={'Nail Clippers'}/>
+    <GroceryListItem item={'Socks'}/>
+  </ul>
+);
+
+let GroceryListItem = (props) => (
+  <li>{props.item}</li>
+);
+
+let Socks = () => (
+  <li>Socks</li>
+);
+
+let NailClippers = () => (
+  <li>Nail Clippers</li>
+);
+
+ReactDOM.render(<GroceryList />, document.getElementById('app'));
